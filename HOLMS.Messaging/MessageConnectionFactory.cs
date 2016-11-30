72afc2805200d941abffd4653e9de396ca81ac11
Derek Lax <lax.derek@gmail.com>
2016-11-30 16:56:43 -0500

    Adds vhost to MessageConnectionFactory
    [#134606283]

--- HOLMS.Messaging/MessageConnectionFactory.cs
+++ HOLMS.Messaging/MessageConnectionFactory.cs
@@ -7,20 +7,23 @@ namespace HOLMS.Messaging {
         private readonly ConnectionFactory _rabbitcf;
         public string Hostname { get; }
 
-        public MessageConnectionFactory(ILogger l, string hostname, string username = null, string password = null) {
+        public MessageConnectionFactory(ILogger l, string hostname, string username, string password, string vhost) {
             _l = l;
             Hostname = hostname;
             _rabbitcf = new ConnectionFactory {
                 HostName = hostname,
             };
             //It appears that using the setter at all, even if the argument is null,
-            //attempts to use the credentials. Instead, we ensure that the properties
+            //attempts to use the value. Instead, we ensure that the properties
             //are only set when they are not null
             if (username != null) {
                 _rabbitcf.UserName = username;
             }
             if (password != null) {
                 _rabbitcf.Password = password;
+            }
+            if (vhost != null) {
+                _rabbitcf.VirtualHost = vhost;
             }
         }
 
